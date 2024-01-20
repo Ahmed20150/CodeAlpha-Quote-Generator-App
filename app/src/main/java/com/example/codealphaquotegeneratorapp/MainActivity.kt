0fd835abdc73305e25167a7aa9bc840bfda8ac38
Ahmed Gasser <ahmed1.gasser@gmail.com>
2024-01-20 12:20:09 +0200

    Initial UI Design Complete

--- app/src/main/java/com/example/codealphaquotegeneratorapp/MainActivity.kt
+++ app/src/main/java/com/example/codealphaquotegeneratorapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.codealphaquotegeneratorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val quizButton: Button = findViewById(R.id.generateButton)
+
+        quizButton.setOnClickListener{
+            generateQuote()
+        }
+    }
+
+
+    fun generateQuote(){
+
     }
 }
